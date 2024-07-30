a3c8fccc029680df1845fa1c5e3a83e3b2130bfb
Abyssal Wolfe <wolfe@the.abyssal.one>
2024-07-30 13:53:52 -0400

    Updated play.sh
    Replaced test commands with []
    Fixed clearing watched videos
    
    Signed-off-by: Abyssal Wolfe <wolfe@the.abyssal.one>

--- shell/play.sh
+++ shell/play.sh
@@ -12,26 +12,23 @@
 #####################################
 
 # Check for existing MPV instance using mpv.sock
-if test -e "/tmp/mpv.sock" && echo "{\"command\": [\"get_version\"]}" | socat - /tmp/mpv.sock | grep -q "success"; then
+if [ -e "/tmp/mpv.sock" ] && echo "{\"command\": [\"get_version\"]}" | socat - /tmp/mpv.sock | grep -q "success"; then
 	# Clear played files from playlist
 	playlist_length=$(echo "{\"command\": [\"get_property\", \"playlist-count\"]}" | socat - /tmp/mpv.sock | jq '.data')
 	playlist_position=$(echo "{\"command\": [\"get_property\", \"playlist-pos\"]}" | socat - /tmp/mpv.sock | jq '.data')
 	playing_eof=$(echo "{\"command\": [\"get_property\", \"eof-reached\"]}" | socat - /tmp/mpv.sock | jq '.data')
 
-	if test "$((playlist_length - 1))" -eq "$playlist_position" && test "$playing_eof" = "true"; then
-		echo "{\"command\": [\"playlist-clear\"]}" | socat - /tmp/mpv.sock
-	else
-		for i in $(seq 0 "$((playlist_length - 1))"); do
-			if test "$i" -lt "$playlist_position"; then
-				echo "{\"command\": [\"playlist-remove\", \"$i\"]}" | socat - /tmp/mpv.sock
-			fi
-		done
-	fi
+	for i in $(seq 0 "$((playlist_length - 1))"); do
+		if [ "$i" -eq "$playlist_position" ] && [ "$playing_eof" != "true" ]; then
+			break
+		fi
+		echo "{\"command\": [\"playlist-remove\", \"$i\"]}" | socat - /tmp/mpv.sock
+	done
 	unset playlist_length playlist_position playing_eof i
 
 	# Append to playlist
 	for url in "$@"; do
-		if ! test -n "$url"; then
+		if [ -z "$url" ]; then
 			continue
 		fi
 		echo "{\"command\": [\"loadfile\", \"$url\", \"append-play\"]}" | socat - /tmp/mpv.sock
